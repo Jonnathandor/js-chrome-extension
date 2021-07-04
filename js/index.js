918d9795750b5dbf9d0f3b697e0fb6f7066010c1
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-07-04 15:26:59 -0500

    added name update feature

--- js/index.js
+++ js/index.js
@@ -89,13 +89,56 @@ const add = (title, icon, url) => {
     });
 }
 
-const getItems = () => {
-    chrome.storage.sync.get(['items'], (data) => {
+const getInitialData = () => {
+    chrome.storage.sync.get(['items', 'name'], (data) => {
         let items = data.items;
+        let name = data.name
         items.forEach(item => {
             renderItem(item.id, item.title, item.icon, item.url);
         });
+
+        setUserName(name);
+    });
+
+}
+
+const updateNameListener = () => {
+    const greetingName = document.querySelector('.greeting__name');
+    greetingName.addEventListener('click', () => {
+        chrome.storage.sync.get(['name'], (data) => {
+            let name = data.name ? data.name : '';
+            document.getElementById('input__name').value = name;
+        })
+        $('#update__name--modal').modal('show');
+        updateName();
     });
 }
 
-getItems();
+const updateName = () => {
+    let changeNameButton = document.querySelector('#update__name');
+
+    changeNameButton.addEventListener('click', (e) => {
+        const userName = document.querySelector('#input__name').value; 
+        if(userName){
+            saveName(userName, () => {
+                setUserName(userName);
+                $('#update__name--modal').modal('hide');
+            });
+        }
+    });
+}
+
+const saveName = (name, callback) => {
+    chrome.storage.sync.set({
+        name: name
+    }, callback);
+}
+
+const setUserName = (name = 'Add Your Name') => {
+    // let userName = name ? name : 'Add Your Name';
+    document.querySelector('.name__value').innerText = name;
+}
+
+
+getInitialData();
+updateNameListener();
