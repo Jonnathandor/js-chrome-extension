77ffc34e35a869a3fb202b85e6c1e4e82436f97e
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-07-04 14:10:19 -0500

    added delete feature

--- js/index.js
+++ js/index.js
@@ -1,35 +1,73 @@
 const addButton = document.getElementById('save__link');
 addButton.addEventListener('click', () => {
-    add('Save For Later!!');
-    renderItem('save');
+    getCurrentTab().then(tab => {
+        add(tab.title, tab.favIconUrl, tab.url);
+    }); 
 });
 
-const renderItem = (text) => {
+async function getCurrentTab() {
+    return await new Promise((resolve, reject)=>{
+      chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+        function(tabs){
+          resolve(tabs[0]);
+        }
+      );
+    })
+}
+
+const deleteItemEvent = (e) => {
+    const id = e.target.parentElement.getAttribute('item-id');
+    const parent = e.target.parentElement;
+
+    chrome.storage.sync.get(['items'], (data) => {
+        let items = data.items;
+
+        const filteredItems = items.filter(item => {
+            return item.id !== id;
+        });
+        
+        chrome.storage.sync.set({
+            items: filteredItems
+        }, () => {
+            parent.remove();
+        });
+    });
+}
+
+const renderItem = (id, title, iconUrl, url) => {
     let itemContainer = document.querySelector('.item__container');
 
     let actionItem = document.createElement('div');
     actionItem.classList.add('action__item');
+    actionItem.setAttribute('item-id', id);
+
+    let itemIcon = document.createElement('div');
+    itemIcon.classList.add('action__item--icon');
 
     let itemText = document.createElement('div');
     itemText.classList.add('action__item--text');
 
     let deleteItem = document.createElement('div');
     deleteItem.classList.add('action__item--delete');
+    deleteItem.addEventListener('click', deleteItemEvent);
 
-    itemText.textContent = text;
-
+    itemIcon.innerHTML = `<img src="${iconUrl}">`;
+    itemText.innerHTML = `<a href="${url}" target="_blank">${title}</a>`;
     deleteItem.innerHTML = `<i class="fas fa-times"></i>`;
 
+    actionItem.appendChild(itemIcon);
     actionItem.appendChild(itemText);
     actionItem.appendChild(deleteItem);
     itemContainer.appendChild(actionItem);
 }
 
-const add = (text) => {
+const add = (title, icon, url) => {
     let item = {
         id: uuidv4(),
         added: new Date().toString(),
-        text: text
+        title: title,
+        icon: icon,
+        url: url
     }
 
     chrome.storage.sync.get(['items'], (data) => {
@@ -44,7 +82,8 @@ const add = (text) => {
             items
         }, () => {
             chrome.storage.sync.get('items', data => {
-                console.log(data);
+                let lastItem = data.items[items.length - 1];
+                renderItem(item.id, lastItem.title, lastItem.icon, lastItem.url);
             });
         });
     });
@@ -54,7 +93,7 @@ const getItems = () => {
     chrome.storage.sync.get(['items'], (data) => {
         let items = data.items;
         items.forEach(item => {
-            renderItem(item.text);
+            renderItem(item.id, item.title, item.icon, item.url);
         });
     });
 }
