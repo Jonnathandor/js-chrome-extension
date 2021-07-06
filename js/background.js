8fa71ef7bee6d3269cf193a2d1133febe8325da4
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-07-05 20:07:33 -0500

    cleaned the background.js

--- js/background.js
+++ js/background.js
@@ -1,6 +1,5 @@
 chrome.runtime.onInstalled.addListener(details => {
     if(details.reason == 'install'){
-        console.log('installed');
         chrome.storage.sync.set({
             items: []
         });
