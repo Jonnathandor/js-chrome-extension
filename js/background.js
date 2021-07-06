4c4a9978147b49d5c3f078a711c8632261fd3386
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-07-05 20:12:38 -0500

    removed background from manifest

--- js/background.js
+++ js/background.js
@@ -1,3 +1,5 @@
+//adding the background to the manifest breaks the delete functionality...
+// I will debug this ... some day
 chrome.runtime.onInstalled.addListener(details => {
     if(details.reason == 'install'){
         chrome.storage.sync.set({
